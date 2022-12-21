f2f6e18eb25bd2c1c4d29efbef673f8b6f58ae78
aemorozov <mclenina19@gmail.com>
2022-12-21 21:38:40 +0400

    chore: add new animation shift

--- js/printingText.js
+++ js/printingText.js
@@ -1,12 +1,16 @@
 printingText();
 function printingText() {
     var timeForStartAnimation = 3500;
-    var timeForAddNewText = 100;
-    var timeForStartScale = 4000;
+    var timeForAddNewText = 80;
+    var timeForStartShift = 3500;
     var newText = ['Welcome to my web-site'];
     var colors = ['#4285f4', '#db4437', '#f4b400', '#0f9d58'];
     var img = document.querySelector('.start-logo');
-    var divStart = document.querySelector('.start');
+    var startTitle = document.querySelector('.start-title');
+    var startP = document.querySelectorAll('.start-p');
+    var startA = document.querySelector('.start-a');
+    var startPLittle = document.querySelector('.start-p-little');
+    var startButton = document.querySelector('.start-button');
     setTimeout(animation, timeForStartAnimation);
     // Functions
     function animation() {
@@ -15,7 +19,7 @@ function printingText() {
             return;
         var spansArray = title.childNodes;
         addNewText(spansArray);
-        setTimeout(scale, timeForStartScale);
+        setTimeout(shift, timeForStartShift);
     }
     function addNewText(spansArray) {
         var i = 0;
@@ -24,7 +28,7 @@ function printingText() {
         img.src = 'img/starts-logo-with-smile.png';
         var addText = setInterval(function () {
             if (i < spansArray.length) {
-                spansArray[i].innerHTML = newText[0][i] || ' .';
+                spansArray[i].innerHTML = newText[0][i] || '';
                 spansArray[i].style.color = colors[Math.floor(Math.random() * colors.length)];
                 i++;
             }
@@ -33,10 +37,16 @@ function printingText() {
             }
         }, timeForAddNewText);
     }
-    function scale() {
-        if (divStart === null)
-            return;
-        divStart.style.transform = ('translateX(-60%) scale(1000)');
-        setTimeout(function () { divStart.style.display = 'none'; }, 2000);
+    function shift() {
+        if (startTitle && startP && startA && startPLittle && startButton && img) {
+            startTitle.style.marginLeft = '-150vw';
+            startA.style.marginLeft = '-150vw';
+            startPLittle.style.marginLeft = '-150vw';
+            startButton.style.marginLeft = '-150vw';
+            img.style.marginLeft = '45%';
+            for (var i = 0; i < startP.length; i++) {
+                startP[i].style.marginLeft = '-150vw';
+            }
+        }
     }
 }
--- js/printingText.ts
+++ js/printingText.ts
@@ -3,13 +3,17 @@ printingText()
 function printingText() {
 
     const timeForStartAnimation = 3500
-    const timeForAddNewText = 100
-    const timeForStartScale = 4000
+    const timeForAddNewText = 80
+    const timeForStartShift = 3500
     const newText = ['Welcome to my web-site']
     const colors = ['#4285f4', '#db4437', '#f4b400', '#0f9d58']
     const img: HTMLImageElement | null = document.querySelector('.start-logo')
-    const divStart: HTMLDivElement | null = document.querySelector('.start')
-
+    const startTitle: HTMLHtmlElement | null = document.querySelector('.start-title')
+    const startP: NodeListOf<HTMLHtmlElement> | null = document.querySelectorAll('.start-p')
+    const startA: HTMLHtmlElement | null = document.querySelector('.start-a')
+    const startPLittle: HTMLHtmlElement | null = document.querySelector('.start-p-little')
+    const startButton: HTMLHtmlElement | null = document.querySelector('.start-button')
+    
     setTimeout(animation, timeForStartAnimation)
 
 
@@ -20,7 +24,7 @@ function printingText() {
         if (title === null) return
         const spansArray = title.childNodes
         addNewText(spansArray)
-        setTimeout(scale, timeForStartScale)
+        setTimeout(shift, timeForStartShift)
         
     }
 
@@ -30,7 +34,7 @@ function printingText() {
         img.src = 'img/starts-logo-with-smile.png'
         const addText = setInterval(() => {
             if (i < spansArray.length) {
-                spansArray[i].innerHTML = newText[0][i] || ' .'
+                spansArray[i].innerHTML = newText[0][i] || ''
                 spansArray[i].style.color = colors[Math.floor(Math.random() * colors.length)]
                 i++
             } else {
@@ -39,9 +43,16 @@ function printingText() {
         }, timeForAddNewText)
     }
 
-    function scale() {
-        if (divStart === null) return
-        divStart.style.transform = ('translateX(-60%) scale(1000)')
-        setTimeout(() => {divStart.style.display = 'none'}, 2000)
+    function shift() {
+        if (startTitle && startP && startA && startPLittle && startButton && img) {
+            startTitle.style.marginLeft = '-150vw'
+            startA.style.marginLeft = '-150vw'
+            startPLittle.style.marginLeft = '-150vw'
+            startButton.style.marginLeft = '-150vw'
+            img.style.marginLeft = '45%'
+            for(let i = 0; i < startP.length; i++) {
+                startP[i].style.marginLeft = '-150vw'
+            }
+        }
     }
 }
